b80aef6beaa4e83828e8bb9d4b44b34c312b0a38
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-31 00:13:33 +0100

    config dep module

--- app/components/utils/utils.module.ts
+++ app/components/utils/utils.module.ts
@@ -2,6 +2,9 @@ module app.utils {
 
     import IModule = angular.IModule;
     var utils:IModule = angular.module('app.utils', [] )
+       .config(()=>{
+            console.log ( 'config utils' ); 
+       })
         .value( 'version', 'v1.0')
         .constant( 'author', 'netTrek')
     ;
