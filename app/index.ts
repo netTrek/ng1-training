3cac9bfe0be65dfa440710fef43e2c266a9bee4a
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-30 23:48:51 +0100

    run app

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,12 @@
 /// <reference path="../typings/main.d.ts" />
 
 import IModule = angular.IModule;
+import {runBlock} from "./index.run";
 
 module app {
 
-    var app:IModule = angular.module('app', [] );
-    console.info ( app );
+    var app:IModule = angular.module('app', [] )
+                             .run( runBlock )
+        ;
 
 }
