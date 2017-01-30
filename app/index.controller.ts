20e2c0a2ba7646dd52bbbb5b0ff6846e3a897d4d
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-31 00:24:56 +0100

    scope methoden

--- app/index.controller.ts
+++ app/index.controller.ts
@@ -1,12 +1,24 @@
 
+interface IAppControllerScope extends angular.IScope {
+  sayYourName (): void;
+}
+
 /** @ngInject */
 export class AppController {
 
   userName:string = "Ich bin der AppController";
 
-  constructor( private $scope:angular.IScope, private $element:angular.IAugmentedJQuery, private $log:angular.ILogService) {
-    $log.log ( $scope , $element);
+  constructor( private $scope:IAppControllerScope, private $element:angular.IAugmentedJQuery, private $log:angular.ILogService) {
+
+    $log.debug ( $scope , $element);
+
+    this.init ();
+
   }
 
-
+  private init () {
+    this.$scope.sayYourName = () => {
+      this.$log.debug ( 'I´m the AppCtroller');
+    }
+  }
 }
