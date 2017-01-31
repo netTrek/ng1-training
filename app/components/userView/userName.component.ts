3167668d3ab93a746eabfbec5f442580ea26176d
Saban Uenlue - netTrek <us@netTrek.de>
2017-01-31 09:15:17 +0100

    comp_fct

--- app/components/userView/userName.component.ts
+++ app/components/userView/userName.component.ts
@@ -6,9 +6,28 @@ import { IUserVO } from './user.module';
 export function userName(): angular.IComponentOptions {
 
     return {
-        template:   '<input type="text" ng-model="userNameCtrl.user.firstname">' +
-                    '<input type="text" ng-model="userNameCtrl.user.lastname">' +
-                    '<span ng-transclude=""></span>',
+        template: ($element:angular.IAugmentedJQuery)=>{
+            console.warn( $element );
+
+            let inputs:Array<{type:string, modelExp:string}> = [    {type:'text', modelExp:'userNameCtrl.user.firstname'},
+                                                                    {type:'text', modelExp:'userNameCtrl.user.lastname'}
+                ],
+                lng: number = inputs.length
+                ;
+            var out:string ='<p>',
+                i = 0,
+                item:{type:string, modelExp:string}
+                ;
+
+            for ( i; i<lng; i++) {
+                item = inputs[i];
+                out += '<input type="'+item.type+'" ng-model="'+item.modelExp+'">';
+            }
+
+            out += '</p><p ng-transclude></p>';
+            return out;
+
+        },
         controller: UserNameCtrl,
         controllerAs: 'userNameCtrl',
         transclude: true,
