1ce9a7184e7f73c23830cce4ccfff58c360a62e1
Saban Uenlue - netTrek <us@netTrek.de>
2017-02-01 12:57:56 +0100

    tipss

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,9 @@
 import IProvideService = angular.auto.IProvideService;
+import ICompileProvider = angular.ICompileProvider;
 /** @ngInject */
-export function configBlock ( $provide: IProvideService ) {
+export function configBlock ( $provide: IProvideService, $compileProvider : ICompileProvider ) {
     console.log ( 'configBlock' );
+    $compileProvider.debugInfoEnabled( false );
     $provide.constant( 'version', 12.3 );
     $provide.value( 'author', 'saban uenlue' );
 }
